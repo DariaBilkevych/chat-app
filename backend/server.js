a6f63d30193ccf1034c14b3a8ac49aed46975e0c
Дар`я Бількевич <ipz202_bdv@student.ztu.edu.ua>
2024-08-27 01:13:52 +0300

    Added settings for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
+import path from 'path';
 import authRoutes from './routes/auth-routes.js';
 import chatRoutes from './routes/chat-routes.js';
 import messageRoutes from './routes/message-routes.js';
@@ -8,6 +9,8 @@ import connectToMongoDB from './db/connectToMongoDB.js';
 import { app, server } from './socket/socket.js';
 
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
 
 dotenv.config();
 
@@ -17,6 +20,12 @@ app.use(cookieParser());
 app.use('/api/auth', authRoutes);
 app.use('/api/chats', chatRoutes);
 app.use('/api/messages', messageRoutes);
+
+app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
+});
 
 server.listen(PORT, () => {
   connectToMongoDB();
